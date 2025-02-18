ec4dbf81330b031de8c02cfd04873efb7bd3bb0e
Eric <127228647+w1a2b3c@users.noreply.github.com>
2025-02-18 20:15:49 +0800

    Add files via upload

---
+++ Db.php
@@ -0,0 +1,14 @@
+<?php
+
+// 数据库操作类
+include 'DbClass.php';
+
+// 数据库配置
+$config = [
+    'db_host' => '127.0.0.1',
+    'db_port' => 3306,
+    'db_name' => '9_71jc_cn',
+    'db_user' => '9_71jc_cn',
+    'db_pass' => 'WEBaK7e2aMYYPPA3'
+];
+?>
